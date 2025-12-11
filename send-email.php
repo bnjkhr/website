556fce3689e40c93e2f824d32c3e06f96685a96f
Ben Kohler <ben.kohler@me.com>
2025-12-11 14:21:59 +0100

    fix: update contact form recipient to ben.kohler@me.com

--- send-email.php
+++ send-email.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 // Resend API Konfiguration
 // API Key muss als Umgebungsvariable auf dem Server gesetzt werden
 $resend_api_key = getenv('RESEND_API_KEY');
-$to = 'mail@benkohler.de';
+$to = 'ben.kohler@me.com';
 $from = 'website@benkohler.de';
 
 // Prüfen ob API Key vorhanden
